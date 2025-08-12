e2c61a8bc8ac2373b6bd7cbe9c70580360e203b8
ALDY <foxeesaga12@gmail.com>
2025-08-12 10:01:29 +0700

    [DONE]

--- migrations/1754058297030_albums.js
+++ migrations/1754058297030_albums.js
@@ -1,13 +1,4 @@
-/**
- * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
- */
-exports.shorthands = undefined;
 
-/**
- * @param pgm {import('node-pg-migrate').MigrationBuilder}
- * @param run {() => void | undefined}
- * @returns {Promise<void> | void}
- */
 exports.up = (pgm) => {
   pgm.createTable('albums', {
     id: {
@@ -25,11 +16,7 @@ exports.up = (pgm) => {
   });
 };
 
-/**
- * @param pgm {import('node-pg-migrate').MigrationBuilder}
- * @param run {() => void | undefined}
- * @returns {Promise<void> | void}
- */
+
 exports.down = (pgm) => {
     pgm.dropTable('albums');
 };
--- migrations/1754232901692_create-table-songs.js
+++ migrations/1754232901692_create-table-songs.js
@@ -1,13 +1,4 @@
-/**
- * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
- */
-exports.shorthands = undefined;
 
-/**
- * @param pgm {import('node-pg-migrate').MigrationBuilder}
- * @param run {() => void | undefined}
- * @returns {Promise<void> | void}
- */
 exports.up = (pgm) => {
   pgm.createTable("songs", {
     id: {
@@ -41,11 +32,7 @@ exports.up = (pgm) => {
   });
 };
 
-/**
- * @param pgm {import('node-pg-migrate').MigrationBuilder}
- * @param run {() => void | undefined}
- * @returns {Promise<void> | void}
- */
+
 exports.down = (pgm) => {
   pgm.dropTable("songs");
 };
