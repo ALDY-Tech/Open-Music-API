2f184d7a196b4110dbec68c6587a7d64555f0324
ALDY <foxeesaga12@gmail.com>
2025-09-17 22:10:17 +0700

    [FIX]Validasi year Album and Songs

--- src/validator/albums/schema.js
+++ src/validator/albums/schema.js
@@ -1,12 +1,12 @@
 const Joi = require('joi');
 
+const currentYear = new Date().getFullYear();
 const AlbumPayloadSchema = Joi.object({
   name: Joi.string().required(),
-  year: Joi.number().required(),
+  year: Joi.number().integer().min(1900).max(currentYear).required(),
 });
 
 const ImageHeadersSchema = Joi.object({
-  //  MIME types dari images.
   'content-type': Joi.string().valid('image/apng', 'image/avif', 'image/gif', 'image/jpeg', 'image/png', 'image/webp').required(),
 }).unknown();
 
